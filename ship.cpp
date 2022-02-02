617d80fad80c7ec0df4e398c509da1e0ce6f6fb3
Landen Doty <landot2002@gmail.com>
2022-02-02 02:40:28 -0600

    adding ship class. Fully tested

---
+++ ship.cpp
@@ -0,0 +1,75 @@
+/** ------------------------------------------------------------------------
+ * @file : ship.cpp
+ * @author : Landen Doty
+ * @assignment : EECS448 - Project 1
+ * @brief : Simple class to define and store ship structure. Should be
+            instantiated from the board class to implement with board
+ * @date: 02-02-22
+ *
+ ------------------------------------------------------------------------ **/
+#include <iostream>
+#include <string>
+#include "ship.h"
+
+Ship::Ship(int size)
+{
+  if(size > 5 || size < 0)
+  {
+    throw(std::runtime_error("Ships must be of size 1-5\n"));
+  }
+  else
+  {
+    m_ship = new char[size];
+    m_size = size;
+    for(int i=0; i<m_size; i++)
+    {
+      m_ship[i] = 's';
+    }
+    m_sunk = false;
+  }
+}
+
+void Ship::hit(int position)
+{
+  if(size > 5 || size < 0)
+  {
+    throw(std::runtime_error("Ships must be of size 1-5\n"));
+  }
+  else
+  {
+    m_ship[position] = '*';
+  }
+}
+
+char* Ship::get_ship()
+{
+  char* return_ship;
+  return_ship = new char[m_size];
+  for(int i=0; i<m_size; i++)
+  {
+    return_ship[i] = m_ship[i];
+  }
+  return(return_ship);
+}
+
+int Ship::get_size()
+{
+  return(m_size);
+}
+
+bool Ship::is_sunk()
+{
+  for(int i=0; i<m_size; i++)
+  {
+    if(m_ship[i] != '*')
+    {
+      m_sunk = true;
+    }
+  }
+  return(m_sunk);
+}
+
+Ship::~Ship()
+{
+  delete m_ship;
+}
---
+++ ship.h
@@ -0,0 +1,71 @@
+/** ------------------------------------------------------------------------
+ * @file : ship.h
+ * @author : Landen Doty
+ * @assignment : EECS448 - Project 1
+ * @brief : Simple class to define and store ship structure. Should be
+            instantiated from the board class to implement with board
+ * @date: 02-02-22
+ *
+ ------------------------------------------------------------------------ **/
+#ifndef SHIP_H
+#define SHIP_H
+
+class Ship
+{
+  public:
+    /**
+       * @pre : None
+       * @post : Ship array created and m_sunk set to false
+       * @param : int size
+       * @throw : runtime error if size less than 0 or larger than 5
+                  is provided
+       * @retun : N/A
+    **/
+    Ship(int size);
+    /**
+       * @pre : Ship array is defined
+       * @post : Ship array pointer delete
+       * @param : None
+       * @throw : None
+       * @retun : None
+    **/
+    ~Ship();
+    /**
+       * @pre : Ship array is defined
+       * @post : Ship array updated at position to *
+       * @param : int position
+       * @throw : runtime error if position is out of bounds
+       * @retun : None
+    **/
+    void hit(int position);
+    /**
+       * @pre : Ship array is defined
+       * @post : Copy of ship array returned
+       * @param : None
+       * @throw : None
+       * @retun : Pointer to a copy of the ship array
+    **/
+    char* get_ship();
+    /**
+       * @pre : Ship array is defined
+       * @post : Ship size returned
+       * @param : None
+       * @throw : None
+       * @retun : m_size
+    **/
+    int get_size();
+    /**
+       * @pre : Ship array is defined
+       * @post : Boolean if ship is sunk (all *'s)
+       * @param : None
+       * @throw : None
+       * @retun : m_sunk
+    **/
+    bool is_sunk();
+  private:
+    int m_size;
+    char* m_ship;
+    bool m_sunk;
+};
+
+#endif
