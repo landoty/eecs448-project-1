0ec7419553b1706cdd7f0e195d2893bb96b8a05a
Duy Vu <d053v574@ku.edu>
2022-02-07 17:27:29 -0600

    Board.cpp file implemented with placeShip method in order to create ships on game board. isValidSpace function created and used within placeShip.

--- board.cpp
+++ board.cpp
@@ -1,21 +1,24 @@
+/** ------------------------------------------------------------------------
+ * @file : Board.cpp
+ * @author : Duy Vu
+ * @assignment : EECS448 - Project 1
+ * @brief : Class that defines the game board. Is able to create, print, and update board.
+ * @date: 02-07-22
+ *
+ ------------------------------------------------------------------------ **/
+
 #include "Board.h"
+#include <iostream>
+#include "ship.h"
 
-Board::Board()
+Board::Board()			
 {
-	m_boardSize = 10;
+	m_boardSize = 10;	//board size is always 10
 	board = nullptr;
+	board = new char*[m_boardSize];
 	for (int i=0; i<m_boardSize; i++)
-	{
-		
-	}
-}
-
-void Board::createBoard(int size)
-{
-	board = new char*[size];
-	for (int i=0; i<size; i++)
-	{
-		board[i] = new char[size];
+	{									//2D array is created to represent board
+		board[i] = new char[m_boardSize];		//Top left: 0,0		Bottom right: 9, 9	when size is 10
 	}
 	
 	char entry = '-';
@@ -23,35 +26,112 @@ void Board::createBoard(int size)
 	{
 		for (int j=0; j<m_boardSize; j++)
 		{
-			board[i][j] = entry;
+			board[i][j] = entry;			//2D array filled with dashes
 		}
 	}
 }
 
+Board::~Board()
+{
+	for (int i=0; i<m_boardSize; i++)	//2D array of board is deleted
+	{
+		delete[] board[i];
+	}
+	delete[] board;
+}
+
 void Board::printBoard()
 {
-	int row = 0;
+	int row = 0;						//row and col are used to print out board coordinates on the side of the board
 	char col = 'A';
 	std::cout << "*";
 	for (int i=0; i<m_boardSize; i++)
 	{
-		std::cout << col;
+		std::cout << col;				//Letters A-J are printed at top of board
 		col++;
 	}
 	std::cout << '\n';
 	for (int i=0; i<m_boardSize; i++)
 	{
-		std::cout << row;
+		std::cout << row;					//Numbers 0-9 are printed on side of board
 		row++;
 		for (int j=0; j<m_boardSize; j++)
 		{
-			std::cout << board[i][j];
+			std::cout << board[i][j];		//Actual board is printed out
 		}
 		std::cout << '\n';
 	}
 }
 
-void Board::updateBoard(int x, int y, char entry)
+void Board::updateBoard(int col, int row, char entry)	//Allows for any coordinate on board to be updated
 {
-	board[x][y] = entry;
+		board[row][col] = entry;					
+	
+}
+
+void Board::placeShip(Ship& entry, int col, int row, char direction)  
+{
+	int increment = 0;
+	if(direction == 'h')			//Horizontal orientation
+	{
+		if(col+entry.get_size()-1 < 10)			//Makes sure ship doesn't get placed outside of board
+		{
+			for (int i=0; i<entry.get_size(); i++)
+			{				
+				if(isValidSpace(col, row))
+				{
+					updateBoard(col, row, entry.get_ship()[increment]);		//Ship is placed
+					col++;
+					increment++;
+				}
+				else
+				{
+					throw(std::runtime_error("Invalid space."));
+				}
+			}
+		}
+		else
+		{
+			throw(std::runtime_error("Ship can not be placed out of bounds."));
+		}
+	}
+	else if(direction == 'v')			
+	{
+		if(row+entry.get_size()-1 < 10)
+		{
+			for (int i=0; i<entry.get_size(); i++)
+			{				
+				if(isValidSpace(col, row))
+				{					
+					updateBoard(col, row, entry.get_ship()[increment]);
+					row++;
+					increment++;
+				}
+				else
+				{
+					throw(std::runtime_error("Invalid space."));
+				}
+			}
+		}
+		else
+		{
+			throw(std::runtime_error("Ship can not be placed out of bounds."));
+		}
+	}
+	else
+	{
+		throw(std::runtime_error("Invalid direction."));
+	}
+}
+
+bool Board::isValidSpace(int col, int row)
+{
+	if(board[row][col] == '-')		//Checks if coordinate currently has no ships
+	{
+		return(true);
+	}
+	else
+	{
+		return(false);
+	}
 }
