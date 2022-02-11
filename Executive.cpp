cf8e3cf824bd1fb0b7d99f718e6605cc16737c00
Aidan Bowen <a047b970@ku.edu>
2022-02-10 20:14:53 -0600

    Enhanced ship placement and made player turn loop

--- Executive.cpp
+++ Executive.cpp
@@ -10,6 +10,7 @@
  #include "Executive.h"
  #include "board.h"
  #include "ship.h"
+ #include "game.h"
  #include <stdexcept>
  #include <iostream>
  
@@ -20,53 +21,112 @@
 	 shipNumber = 0;
  }
  
- Executive::play()
+ void Executive::play()
  {
-	 game gameFunction;
+	 Game gameFunction;
 	 std::cout<<"Welcome to Battleship!\n";
 //	 std::cin>>boardSize;
 //	 creates a board object to make the game board and then displays it
-	 board gameBoardMoves1;
-	 board gameBoardMoves2;          //4 game boards for view and ships
-	 board gameBoardShips1;
-	 board gameBoardShips2;
-	 gameBoardMoves1.createBoard(boardSize);
+	 Board gameBoardMoves1;
+	 Board gameBoardMoves2;          //4 game boards for view and ships
+	 Board gameBoardShips1;
+	 Board gameBoardShips2;
+	 char xCoordinate;
+	 int yCoordinate;
+	 char direction;
+	 std::string Player1 = " ";
+	 std::string Player2 = " ";
+/*	 gameBoardMoves1.createBoard(boardSize);
 	 gameBoardMoves2.createBoard(boardSize);
 	 gameBoardShips1.createBoard(boardSize);
-	 gameBoardShips2.createBoard(boardSize);
+	 gameBoardShips2.createBoard(boardSize);*/
 	 std::cout<<"How many ships will each player get? (1-5)\n";
 	 std::cin>>shipNumber;
 	 //player 1 places ships
 	 std::cout<<"Player 1, place your ships.\n";
+	 /*
 	 try
 	 {
 		for(int i = 1; i<=shipNumber; i++)
 		{
 			gameBoardShips1.printBoard();
 			Ship battleshipShip(i);
-			std::cout<<"Where should your size "<<i<<" ship go? Enter x and y coordinates.\n";
-			gameBoardShips1.placeShip(battleshipShip);
+			std::cout<<"Where should your size "<<i<<" ship go? Enter row, column, and direction.\n";
+			std::cout<<"Row(1-10): ";
+			std::cin>>yCoordinate;
+			std::cout<<"Column(A-J): ";
+			std::cin>>xCoordinate;
+			std::cout<<"Direction(h/v): ";
+			std::cin>>direction;
+			gameBoardShips1.placeShip(battleshipShip, xCoordinate, yCoordinate, direction);
 		}
 	 }
 	 catch(std::exception& e)
 	 {
 		 std::cout<<e.what();
+	 }*/
+	 for(int i = 1; i<=shipNumber; i++)
+	 {
+		gameBoardShips1.printBoard();
+		Ship battleshipShip(i);
+		std::cout<<"Where should your size "<<i<<" ship go? Enter row, column, and direction.\n";
+		std::cout<<"Row(1-10): ";
+		std::cin>>yCoordinate;
+		std::cout<<"Column(A-J): ";
+		std::cin>>xCoordinate;
+		std::cout<<"Direction(h/v): ";
+		std::cin>>direction;
+		try
+		{
+			gameBoardShips1.placeShip(battleshipShip, xCoordinate, yCoordinate, direction);
+		}
+		catch(std::exception& e)
+		{
+			std::cout<<e.what();
+		}
 	 }
 	 //player 2 places ships
 	 std::cout<<"Player 2, place your ships.\n";
+	 /*
 	 try
 	 {
 		for(int i = 1; i<=shipNumber; i++)
 		{
 			gameBoardShips2.printBoard();
 			Ship battleshipShip(i);
-			std::cout<<"Where should your size "<<i<<" ship go? Enter x and y coordinates.\n";
-			gameBoardShips2.placeShip(battleshipShip);
+			std::cout<<"Where should your size "<<i<<" ship go? Enter row, column, and direction.\n";
+			std::cout<<"Row(1-10): ";
+			std::cin>>yCoordinate;
+			std::cout<<"Column(A-J): ";
+			std::cin>>xCoordinate;
+			std::cout<<"Direction(h/v): ";
+			std::cin>>direction;
+			gameBoardShips2.placeShip(battleshipShip, xCoordinate, yCoordinate, direction);
 		}
 	 }
 	 catch(std::exception& e)
 	 {
 		 std::cout<<e.what();
+	 }*/
+	 for(int i = 1; i<=shipNumber; i++)
+	 {
+		gameBoardShips2.printBoard();
+		Ship battleshipShip(i);
+		std::cout<<"Where should your size "<<i<<" ship go? Enter row, column, and direction.\n";
+		std::cout<<"Row(1-10): ";
+		std::cin>>yCoordinate;
+		std::cout<<"Column(A-J): ";
+		std::cin>>xCoordinate;
+		std::cout<<"Direction(h/v): ";
+		std::cin>>direction;
+		try
+		{
+			gameBoardShips2.placeShip(battleshipShip, xCoordinate, yCoordinate, direction);
+		}
+		catch(std::exception& e)
+		{
+			std::cout<<e.what();
+		}
 	 }
 	 //starts game by looping until game over is true
 	 do
@@ -75,10 +135,14 @@
 		 //then asks player two for a move
 		 gameBoardMoves1.printBoard();
 		 gameBoardShips1.printBoard();
-		 gameFunction(Player1);
+		 gameFunction.fire(Player1);
 		 gameBoardMoves2.printBoard();
 		 gameBoardShips2.printBoard();
-		 gameFunction(Player2);
-	 }while(gameOver == false);
+		 gameFunction.fire(Player2);
+	 }while(gameFunction.gameEndCheck() == false);
 	 std::cout<<"Game over!\n";
+ }
+ 
+ Executive::~Executive()
+ {
  }
