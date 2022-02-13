6aa19db23a0b4412abf317977028d847c370e1d6
ezhuo56 <ericzhuo56@gmail.com>
2022-02-12 20:53:20 -0600

    Added comments to fire and end game functions. Validates user input for a specific type of input.

--- game.cpp
+++ game.cpp
@@ -54,6 +54,8 @@ void Game::shipPlacement()
 					std::cout << "Player "<< i <<", where would you like to place ship " << j+1 << "?\n";
           do
           {
+			 std::cin.clear();					//Prompts for input if user gives a non integer
+			std::cin.ignore();
 	          std::cout << "Column: ";
 	          std::cin >> col;
 	          col=tolower(col);
@@ -69,8 +71,10 @@ void Game::shipPlacement()
 
 					do
           {
+			std::cin.clear();					//Prompts for input if user gives a non integer
+			std::cin.ignore();
           	std::cout << "Row: ";
-						std::cin >> row;
+			std::cin >> row;
            	result =(row<=10 && row >=1);
            	if(result == false)
           	{
@@ -125,12 +129,14 @@ void Game::fire(std::string playerName)
 		std::cout << "===============================================\n";
     do
     {
+		std::cin.clear();	//Prompts for input if user gives a non character
+		std::cin.ignore();
     	std::cout << "Column: ";
     	std::cin >> col;
-    	col=tolower(col);
-    	newCol = col;
-    	newCol = newCol - 97;
-    	result =(newCol<=9 && newCol >=0);
+    	col=tolower(col); //change user input char to always lowercase
+    	newCol = col; //set char col to int newCol 
+    	newCol = newCol - 97; //converts it to 0-9 based on ascii value
+    	result =(newCol<=9 && newCol >=0); //checks for the converted ascii value to see if its 0=9 for a-j
     	if(result == false)
     	{
       	std::cout << "Invalid input. Please enter letters a-j.";
@@ -140,9 +146,11 @@ void Game::fire(std::string playerName)
 
     do
     {
+		std::cin.clear();	//Prompts for input if user gives a non integer
+		std::cin.ignore();
 	    std::cout << "Row: ";
 	    std::cin >> row;
-	    result =(row<=10 && row >=1);
+	    result =(row<=10 && row >=1); //check user input to see if its between 1-10
 	    if(result == false)
 	    {
 	          std::cout << "Invalid input. Please enter numbers 1-10.";
@@ -157,20 +165,20 @@ void Game::fire(std::string playerName)
  			{
     		try
     		{
-      		player2_ships[i]->hit(col, row);
+      		player2_ships[i]->hit(col, row); //check location of ship index and throws if the ship is not at location against player 2's placed ships
     		}
     		catch(const std::exception& e)
     		{
-	        missCount++;
+	        missCount++; //increments a counter for misses (1 ship selected = 1 at a specific location and so on...)
     		}
 			}
-    	if(numShips==missCount)
+    	if(numShips==missCount) //if misses equals to the ship index it would mark the specific location with M for miss
     	{
       	player1_eBoard.updateBoard(col,row,missChar);
     	}
 		else
     	{
-      	player1_eBoard.updateBoard(col,row,hitChar);
+      	player1_eBoard.updateBoard(col,row,hitChar); //if the miss counter is not incremented it would mark with * for hit
     	}
 
 			std::cout << "===============================================\n";
@@ -188,20 +196,20 @@ void Game::fire(std::string playerName)
     	{
 	    	try
 	    	{
-	       player1_ships[i]->hit(col, row);
+	       player1_ships[i]->hit(col, row);//check location of ship index and throws if the ship is not at location against player 1's placed ships
 	    	}
 	    	catch(const std::exception& e)
 	    	{
-	        missCount++;
+	        missCount++; //increments a counter for misses (1 ship = 1 at a specific location and so on...)
 	    	}
 			}
 		if(numShips==missCount)
 	    {
-	      player2_eBoard.updateBoard(col,row,missChar);
+	      player2_eBoard.updateBoard(col,row,missChar); //if misses equals to the ship index it would mark the specific location with M for miss
 	    }
 	    else
 	    {
-	      player2_eBoard.updateBoard(col,row,hitChar);
+	      player2_eBoard.updateBoard(col,row,hitChar); //if the miss counter is not incremented it would mark with * for hit
 	    }
 			std::cout << "===============================================\n";
 			std::cout << "Player 2's board\n";
@@ -213,50 +221,50 @@ void Game::fire(std::string playerName)
 }
 bool Game::player1Won()
 {
-    for(int i=0;i<numShips;i++)
+    for(int i=0;i<numShips;i++) //for loops for the amount of ships in the game
     {
-        if(player1_ships[i]->is_sunk()==true)
+        if(player1_ships[i]->is_sunk()==true) //checks the status of the ship if it marked with *
             {
-                player1WonCheck = false;
+                player1WonCheck = false; //tells the game that player 1 has lost
             }
             else
             {
-                player1WonCheck = true;
+                player1WonCheck = true;//the game ends with player 2 winning
             }
     }
-    return player1WonCheck;
+    return player1WonCheck; //returning the true false statement
 }
 
 bool Game::gameEndCheck()
 {
 		bool player1_end = true;
 		bool player2_end = true;
-    for(int i=0;i<numShips;i++)
+    for(int i=0;i<numShips;i++) //loops to check for ship index sunk status
     {
-        if(player1_ships[i]->is_sunk() == false)
+        if(player1_ships[i]->is_sunk() == false) //continues the game if all ships are not sunk from player 1
         {
             player1_end = false;
-						break;
+					break;
         }
 		}
 		for(int i=0;i<numShips;i++)
 		{
-			if(player2_ships[i]->is_sunk()==false)
+			if(player2_ships[i]->is_sunk()==false)//continues the game if all ships are not sunk from player 2
       {
           player2_end = false;
 					break;
       }
 		}
- 	return(player1_end || player2_end);
+ 	return(player1_end || player2_end); //return either the player 1 and 2 boolean
 }
 Game::~Game()
 {
-  	for (int i=0; i<numShips; i++)
+  	for (int i=0; i<numShips; i++) //deletes all indexes of ships
 	{
 		delete player1_ships[i];
 		delete player2_ships[i];
 	}
-	delete[] player1_ships;
-	delete[] player2_ships;
+	delete[] player1_ships; //delete the pointer of ship 1
+	delete[] player2_ships;//delete the pointer of ship 2
 
 }
